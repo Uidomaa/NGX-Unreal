b2edf371a8d90b260db2f4b210e444923134b9c0
Michael Widmer <michaeljwidmer@gmail.com>
2024-04-24 00:13:09 -0700

    - Fixed build issue

--- Source/NGX_Unreal/NGX_UnrealGameMode.cpp
+++ Source/NGX_Unreal/NGX_UnrealGameMode.cpp
@@ -7,8 +7,8 @@
 ANGX_UnrealGameMode::ANGX_UnrealGameMode()
 	: Super()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
+	// // set default pawn class to our Blueprinted character
+	// static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	// DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 }
